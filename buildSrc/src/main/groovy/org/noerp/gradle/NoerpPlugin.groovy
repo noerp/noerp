1244bf38597165d8e4dfdc5564dfe7702a7d7043
kevin <kai.yuan@foxmail.com>
2015-12-25 01:11:54 +0800

    implements vendor method for a project

--- buildSrc/src/main/groovy/org/noerp/gradle/NoerpPlugin.groovy
+++ buildSrc/src/main/groovy/org/noerp/gradle/NoerpPlugin.groovy
@@ -2,29 +2,13 @@ package org.noerp.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
-
-import org.gradle.api.internal.file.FileResolver;
-import org.gradle.internal.reflect.Instantiator;
-
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.GroovyPlugin;
+import org.gradle.api.tasks.Exec
 
 import org.noerp.gradle.task.*
-import org.noerp.gradle.dependency.*
-
-import javax.inject.Inject
 
 class NoerpPlugin implements Plugin<Project> {
-
-	private final Instantiator instantiator
-    private final FileResolver fileResolver
-
-	@Inject
-	NoerpPlugin(Instantiator instantiator, FileResolver fileResolver){
-		this.instantiator = instantiator
-		this.fileResolver = fileResolver
-	}
 
 	void apply (Project project) {
 		init(project)
@@ -44,34 +28,34 @@ class NoerpPlugin implements Plugin<Project> {
 
 		project.subprojects.each {subproject->
 
-			subproject.apply plugin:JavaPlugin
-			subproject.apply plugin:GroovyPlugin
+			subproject.apply(plugin:JavaPlugin)
+			subproject.apply(plugin:GroovyPlugin)
 
-    		subproject.extensions.create("vendor", VendorDependency)
+    		subproject.convention.plugins.noerp = new NoerpPluginConvention(subproject)
 		}
 	}
 
 	void setupTask(Project project){
 		project.task("install", type: InstallTask) {
-			
 		}
 		project.task("update", type: UpdateTask){
-
 		}
-		project.task("start", type: StartTask){
+		project.task("start", type:Exec){
+			commandLine "java -jar bin/noerp.jar"
+			workingDir "."
 		}
 		project.task("start-debug", type: StartTask){
 			debug = true
 		}
 		project.task("stop", type: StopTask){
-			
 		}
+		/*
 		project.task("create-component", type: CreateComponentTask){
 			
 		}
 		project.task("generate-CRUD", type: CreateComponentTask){
 			
-		}
+		}*/
 	}
 
 }
