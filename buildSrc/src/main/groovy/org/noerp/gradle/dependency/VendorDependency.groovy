480bd11acc0db804173fa689cfdffa29a9074976
kevin <kai.yuan@foxmail.com>
2016-04-02 11:36:29 +0800

    remove buildSrc for using git subtree

--- buildSrc/src/main/groovy/org/noerp/gradle/dependency/VendorDependency.groovy
+++
@@ -1,7 +0,0 @@
-package org.noerp.gradle.dependency
-
-import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
-
-class VendorDependency extends DefaultExternalModuleDependency {
-
-}
